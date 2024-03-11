bb1037640f28162e5bcc8ab17e62ed6d99914426
J-Breno <joao.breno85@hotmail.com>
2024-03-11 11:11:20 -0300

    Terminando os exercicios sobre array

--- array/untitled/src/Cardapio.java
+++ array/untitled/src/Cardapio.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Cardapio {
     ItemCardapio[] itens = new ItemCardapio[3];
     int count = 0;
@@ -9,7 +11,11 @@ public class Cardapio {
         itens[indice] = null;
     }
     void imprimirItensCardapio(double precoMinimo, double precoMaximo) {
+        Objects.requireNonNull(precoMinimo, "Deu ruim");
 
+        if(precoMinimo < 0) {
+            throw new IllegalArgumentException("Não pode menor do que 0");
+        }
         for (ItemCardapio iten : itens) {
             if(iten != null) {
                 if(iten.preco >= precoMinimo && iten.preco <= precoMaximo) {
--- array/untitled/src/InicioProgram.java
+++ array/untitled/src/InicioProgram.java
@@ -1,6 +1,6 @@
 
 public class InicioProgram {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //
         Cardapio cardapio = new Cardapio();
 
 
