3ea11f5557cc8e6a6f153f3ca0b30f4e9b72b8c2
J-Breno <joao.breno85@hotmail.com>
2024-03-13 13:59:13 -0300

    Ajeitando umas coisas no exercicio

--- heranca/Heranca/src/Main1.java
+++ heranca/Heranca/src/Main1.java
@@ -3,7 +3,13 @@ import com.github.JBreno.banco.Titular;
 
 public class Main1 {
     public static void main(String[] args) {
-        Conta conta1 = new Conta();
+        Conta conta1 = new Conta(); // se você comparar um objeto com outro usando ==, deesse jeito:
+        //  Conta conta1 = new Conta();
+        //  Conta conta2 = conta1;
+        // eles vão dar iguais, pois o == olha o valor da referncia na memoria
+        // já o Object.equals olha o valor em si, das variaveis, mas você tera que sobrescrever o equals
+
+
         conta1.setTitular(new Titular("João Breno", "xxx.xxx.xxx-xx"));
         conta1.setAgencia(1234);
         conta1.setNumero(999999);
