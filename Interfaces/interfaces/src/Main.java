80e19a6c330ff02fd824236d7ab1907af17d546f
J-Breno <joao.breno85@hotmail.com>
2024-03-14 22:44:47 -0300

    comecando os estudos de interfaces

---
+++ Interfaces/interfaces/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // os contatos, padrões são chamados de interfaces
+        // uma interface é um contrato que especifica o que alguma coisa vai fazer
+
+        // tipo o pedal do freio, ele não faz o carro parar, ele apenas aciona a coisa
+        // que faz o carro parar;
+
+        // as interfaces fazem o meio de campo dessa comunicação
+
+        // outro exemplo é uma tv, que tem um botão de ligar, não é o botão que liga, mas
+        // ele aciona o mecanismo que ligag
+
+        System.out.println("Hello world!");
+    }
+}
